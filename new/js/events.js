675dc399552e10666df865368d03b27566bd1292
nitsan <nitsanzo@gmail.com>
2021-03-12 15:48:06 +0200

    Add send email in contact form

--- new/js/events.js
+++ new/js/events.js
@@ -18,3 +18,28 @@ $("#whatsappChat").click(function () {
 $("#facebookChat").click(function () {
     sendEvent("click", "contact", "facebook");
 })
+
+$('#contact-form').on('submit', function(event) {
+    event.preventDefault(); // prevent reload
+
+    var formData = new FormData(this);
+    formData.append('service_id', 'sendgrid');
+    formData.append('template_id', 'contact');
+    formData.append('user_id', 'user_aOOagFzIvXwNrk0mRZE5S');
+    formData.append('from_name', $('#name').val());
+    formData.append('phone', $('#phone').val());
+    formData.append('from_email', $('#email').val());
+    formData.append('message', $('#message').val());
+
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+        type: 'POST',
+        data: formData,
+        contentType: false, // auto-detection
+        processData: false // no need to parse formData to string
+    }).done(function() {
+        $('#message-sent').removeClass('d-none');
+        $('#contact-form').addClass('d-none');
+    }).fail(function(error) {
+        alert('Oops... ' + JSON.stringify(error));
+    });
+});
